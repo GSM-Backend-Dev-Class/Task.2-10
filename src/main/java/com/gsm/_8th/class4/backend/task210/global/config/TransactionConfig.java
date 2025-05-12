b642d25846014ded1dfe7317c1f7781e0df73296
KIM TAEEUN <snowykte0426@naver.com>
2025-05-13 00:43:49 +0900

    feat: add TransactionConfig for enabling transaction management

---
+++ src/main/java/com/gsm/_8th/class4/backend/task210/global/config/TransactionConfig.java
@@ -0,0 +1,9 @@
+package com.gsm._8th.class4.backend.task210.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class TransactionConfig {
+}
