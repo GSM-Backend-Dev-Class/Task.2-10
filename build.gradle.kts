7df96743d30d235ed8d8a979f63bfd9aa72a93e3
KIM TAEEUN <snowykte0426@naver.com>
2025-05-13 00:42:51 +0900

    feat: add Spring Boot validation starter dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,9 +26,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
-	runtimeOnly("com.mysql:mysql-connector-j")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	runtimeOnly("com.h2database:h2")
