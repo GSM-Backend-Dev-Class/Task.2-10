da94d9becde807888095c78eeb1ff23dca05050a
KIM TAEEUN <snowykte0426@naver.com>
2025-05-12 23:45:39 +0900

    init: initialize Spring Boot application with MySQL configuration and basic test setup

---
+++ src/main/java/com/gsm/_8th/class4/backend/task210/Task210Application.java
@@ -0,0 +1,13 @@
+package com.gsm._8th.class4.backend.task210;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task210Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task210Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsm/_8th/class4/backend/task210/Task210ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsm._8th.class4.backend.task210;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task210ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
